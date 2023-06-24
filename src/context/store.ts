edae5555777b9e0f5c257dbec2ce20bca3a3a2b1
Cupcakes33 <rute930627@gmail.com>
2023-06-24 13:28:10 +0900

    feat: add useCounterStore

---
+++ src/context/store.ts
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+type state = {
+  num: number;
+  increase: (amount: number) => void;
+  remove: () => void;
+};
+
+const useCounterStore = create<state>((set) => ({
+  num: 0,
+  increase: (amount: number) => set((state) => ({ num: state.num + amount })),
+  decrease: (amount: number) => set((state) => ({ num: state.num - amount })),
+  remove: () => set({ num: 0 }),
+}));
+
+export default useCounterStore;
